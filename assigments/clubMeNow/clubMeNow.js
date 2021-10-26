fa26d2bd2f24d28dc333ca7d73dbf094dd1aba58
SuperEliteGamer <90627630+SuperEliteGamer@users.noreply.github.com>
2021-10-25 22:14:52 -0400

    Update clubMeNow.js

--- assigments/clubMeNow/clubMeNow.js
+++ assigments/clubMeNow/clubMeNow.js
@@ -92,12 +92,6 @@ function displayClubDistanceEntryForm(c) {
 // replace the current "clubs" array with the previous one
 function undoLastShot() {
         // your code here !
-	let clubs = obj = JSON.parse(localStorage.slice(localStorage.indexOf("clubs:","clubsUndo:")));
-    	// store the array in local storage
-	let str = JSON.stringify(clubs);
-	localStorage.setItem("clubs", str);
-	// and refresh screen
-	window.location.href = "clubDistanceList.html"; 
 }
 
 // create a new (default) "clubs" array

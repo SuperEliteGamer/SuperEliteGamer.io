b4fbfd5599f8c35e2359cf57698963658f896746
SuperEliteGamer <90627630+SuperEliteGamer@users.noreply.github.com>
2021-11-10 03:59:31 -0500

    Update assignment05.js

--- assigments/assignment05.js
+++ assigments/assignment05.js
@@ -43,7 +43,7 @@ var chartData = {
           // logarithmic scale ignores maxTicksLimit
           maxTicksLimit: 11,
           callback: function(label, index, labels) {
-            return ((Math.log(label)/Math.log(10)) % 1 === 0) ? label/1000+'k' :  null;
+            return ((Math.log(label/1000)/Math.log(10)) % 1 === 0) ? label/1000+'k' :  null;
           }
         },
         scaleLabel: {
